c46f091066d9d6e54aec056fc4f7dc0c73ecd60a
Artem Pyanykh <artem.pyanykh@gmail.com>
2022-01-07 15:20:11 +0000

    Add CLI parsing to the example app

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+$(eval $(ARGS):;@:)
+
 .PHONY: bootstrap
 bootstrap:
 	git submodule update --init && vcpkg/bootstrap-vcpkg.sh
@@ -12,11 +15,11 @@ configure:
 
 .PHONY: build
 build:
-	cmake --build build
+	@cmake --build build
 
 .PHONY: run
 run: build
-	build/main
+	@build/main $(ARGS)
 
 .PHONY: reconfigure
 reconfigure: clean configure
--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
+#include "CLI/App.hpp"
+#include "CLI/Config.hpp"
+#include "CLI/Formatter.hpp"
 #include <fmt/ranges.h>
 #include <vector>
 
+// Forward declarations
+void demo1Handler();
+void demo2Handler();
+
 int main(int argc, char **argv) {
-  fmt::print("Hello CPP\n");
+  CLI::App app{"Template project with cmake and vcpkg"};
+  app.require_subcommand();
+
+  auto demo1 = app.add_subcommand("demo1", "Demo 1");
+  auto demo2 = app.add_subcommand("demo2", "Demo 2");
+
+  CLI11_PARSE(app, argc, argv);
+
+  if (demo1->parsed()) {
+    demo1Handler();
+  }
+
+  if (demo2->parsed()) {
+    demo2Handler();
+  }
+
+  return 0;
+}
+
+void demo1Handler() {
+  fmt::print("Welcome to Demo 1!\n\n");
 
   std::vector<int> vec{1, 2, 3, 4};
   fmt::print("Vec: {}\n", vec);
@@ -18,4 +45,11 @@ int main(int argc, char **argv) {
   closure(7);
 
   fmt::print("Vec: {}\n", vec);
+}
+
+void demo2Handler() {
+  const auto msg = R"(Welcome to Demo 2!
+There's nothing here currently.
+)";
+  fmt::print(msg);
 }
